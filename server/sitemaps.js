d5bb895585624d070618ee1408e515566db95b0b
Daniel Li <dan@danyll.com>
2016-01-15 16:52:48 +0800

    Added sitemap. Resolves #5.

---
+++ server/sitemaps.js
@@ -0,0 +1,66 @@
+sitemaps.add('/sitemap.xml', function() {
+  // required: page
+  // optional: lastmod, changefreq, priority, xhtmlLinks, images, videos
+  return [
+    {
+      page: '/',
+      lastmod: new Date(),
+      changefreq: 'daily',
+      priority: 1
+    },
+    {
+      page: '/contact-us',
+      lastmod: new Date(),
+      changefreq: 'weekly',
+      priority: 0.8
+    },
+    {
+      page: '/projects',
+      lastmod: new Date(),
+      changefreq: 'weekly',
+      priority: 0.4
+    },
+    {
+      page: '/projects/skeduo',
+      lastmod: new Date(),
+      changefreq: 'weekly',
+      priority: 0.1
+    },
+    {
+      page: '/projects/takeoff',
+      lastmod: new Date(),
+      changefreq: 'weekly',
+      priority: 0.1
+    },
+    {
+      page: '/projects/bebello',
+      lastmod: new Date(),
+      changefreq: 'weekly',
+      priority: 0.1
+    },
+    {
+      page: '/projects/techni-photo',
+      lastmod: new Date(),
+      changefreq: 'weekly',
+      priority: 0.1
+    },
+    {
+      page: '/process',
+      lastmod: new Date(),
+      changefreq: 'weekly',
+      priority: 0.5
+    },
+    {
+      page: '/solutions',
+      lastmod: new Date(),
+      changefreq: 'weekly',
+      priority: 0.6
+    },
+    {
+      page: '/services',
+      lastmod: new Date(),
+      changefreq: 'weekly',
+      priority: 0.5
+    },
+  ];
+});
